c6dde598dea0a3d72efa09614dbf73cd6604b7ce
avinash <avi.quid@gmail.com>
2019-03-21 02:09:36 +0530

    add to home screen, name and description changed

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -121,9 +121,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Pet Monitor',
+      short_name: 'PM',
+      description: 'My Pet project!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
